slowllama: fix f16 <-> f32 for loss computation

blackbox_model.py
@@ -326,7 +326,11 @@ class Transformer(nn.Module):
         logging.log(level=logging.DEBUG, msg=f'output layer')
         logits = self.output(norm_out)
         logging.log(level=logging.DEBUG, msg=f'output layer done')
-        logits = logits.to(torch.float32).detach()
+
+        if (self.params.compute_dtype != torch.float32):
+            logits = logits.to(torch.float32)
+
+        logits = logits.detach()
         logits.requires_grad = True
 
         loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1), ignore_index=-1)
@@ -334,7 +338,7 @@ class Transformer(nn.Module):
 
         loss.backward()
 
-        norm_out_grad = self.backprop_w_lora(self.output, logits.grad.to(torch.float16))
+        norm_out_grad = self.backprop_w_lora(self.output, logits.grad.to(self.params.compute_dtype))
         logging.log(level=logging.DEBUG, msg=f'combined: output layer done')
 
         norm_out2 = self.norm(current)